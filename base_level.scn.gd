376405696c33b4b3cb15facaf667f3649a8703b2
Morgawr <morgawr@gmail.com>
2016-04-18 00:52:21 +0100

    Added global audio and toggleable audio button

--- base_level.scn.gd
+++ base_level.scn.gd
@@ -7,7 +7,13 @@ extends Node2D
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	get_node("BGM").play()
+	#get_node("BGM").play()
+	var level_bgm = get_node("/root/LevelBGM")
+	var globals = get_node("/root/globals")
+	if !level_bgm.is_playing():
+		level_bgm.play()
+	if globals.audio_muted:
+		level_bgm.set_paused(true)
 	pass
 
 
---
+++ globals.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+var audio_muted = false
+
