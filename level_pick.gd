0c0b7a70a7cf43483bb77e1be8dd33c50b0f00d3
Ivan Vučica <ivan@vucica.net>
2016-04-17 23:06:38 +0100

    Level 6. Level picker list in separate file.

---
+++ level_pick.gd
@@ -0,0 +1,8 @@
+extends OptionButton
+func _ready():
+	self.add_item("Level 1")
+	self.add_item("Level 2")
+	self.add_item("Level 3")
+	self.add_item("Level 4")
+	self.add_item("Level 5")
+	self.add_item("Level 6")
