bafbcdfc7f8669965f258288a24de4704214f44b
Morgawr <morgawr@gmail.com>
2016-04-18 23:55:03 +0100

    Added transitioning sound

--- exit_level.gd
+++ exit_level.gd
@@ -11,6 +11,8 @@ func exit_level():
 	#get_tree().change_scene(next_level)
 
 	var player = get_tree().get_nodes_in_group("Player")[0]
+	if not get_node("/root/globals").audio_muted:
+		get_node("/root/LevelBGM/SamplePlayer").play("level_change", true)
 	if next_level == "":
 		get_node("/root/LevelBGM").stop()
 		next_level = "res://main_menu.scn.xml"
